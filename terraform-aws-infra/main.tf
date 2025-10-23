d46e13cda2673c1a48c56ed22c0faa4dce71d839
Lucas Bianchin <ldrocha16@gmail.com>
2025-10-22 23:20:40 -0300

    feat: add initial AWS provider and Terraform configuration

---
+++ terraform-aws-infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.13.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "lr0cha"
+      managed-by = "terraform"
+    }
+  }
+}
